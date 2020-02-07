ec1ba22e929a6ade2b87e6621e7c18c475fb61ce
concurrent <concurrent.exec@gmail.com>
2020-02-07 11:34:33 +0800

    read

--- control/read.py
+++ control/read.py
@@ -1,10 +1,15 @@
 #coding=utf-8
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from model.type import FailResp, SuccessResp
+from sql.select import select
 
 read = Blueprint('read', __name__)
 
-@read.route("/api/read", methods=['GET'])
-def index():
-    return "read"
+@read.route("/api/read/<str:city>", methods=['GET'])
+def index(city=""):
+    if city == "":
+        return jsonify(FailResp())
+    data = select(city)
+    return jsonify(SuccessResp(data))
 
 
