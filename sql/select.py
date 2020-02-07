adb7265c9cad274fb652f40e1c657773a2964ced
concurrent <concurrent.exec@gmail.com>
2020-02-07 13:59:06 +0800

    尝试修复bug

--- sql/select.py
+++ sql/select.py
@@ -3,7 +3,7 @@ from model.type import Archive
 from pymysql.cursors import DictCursor
 
 def select(city:str)->[]:
-    with connection.cursor(DictCursor) as cursor:
+    with connection.cursor() as cursor:
         # Create a new record
         sql = "SELECT `province` `city` `publish_time` `publish_date` `title` `content` `link` `links_to_pic` `announce_type`\
             FROM `archives` WHERE `city`=%s"
